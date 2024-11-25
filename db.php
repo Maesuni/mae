fde7625aa56b6071010a6f5f275511c348458e04
Mae <146410195+Maesuni@users.noreply.github.com>
2024-11-25 10:52:22 +0800

    db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "root"; // Adjust based on your setup
-$password = "";
+$username = "mae"; // Adjust based on your setup
+$password = "almonia";
 $database = "mae";
 
 try {
